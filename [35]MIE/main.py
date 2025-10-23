90426c29a823456989eedb0933b0bfcfb38fe1a4
Home <2117409157@qq.com>
2025-10-23 16:50:56 +0800

    写到step6 Scrambling_BitPlane()函数有点小问题

--- [35]MIE/main.py
+++ [35]MIE/main.py
@@ -1,10 +1,65 @@
 import cv2
 from utils import *
+import numpy as np
+import os
+
 
 def just_testmain():
-    Show_Chen()
-    pass
+    imgs = []
+    M, N = 512, 512
+    
+    for i in os.listdir("D:\ImageEncryption\Testimg"):
+        t_img = cv2.imread(os.path.join("D:\ImageEncryption\Testimg", i), cv2.IMREAD_GRAYSCALE)
+        imgs.append(t_img)
+    print("Loaded", len(imgs), "images for testing.")
+    K = len(imgs)
+    #将图像转化为位平面
+    bits_imgs = [Img2Bits(img) for img in imgs]
+    #使用SHA-256生成密钥流
+    hash_seq = Generate_sha256_sequence("Hello, World!")
+    x1, y1, r1, r2, s1, s2, x0, y0, z0 = Generate_KeyStream(hash_seq)
+    
+    #使用x1,y1,r1,r2,s1,s2作为参数，进行二维Logistic映射迭代，两个M*N的混沌序列-X1和X2
+    X1, X2 = [], []
+    for i in range(M*N+3000):
+        x1, y1 = Twodim_logistic_map(x1, y1, r1, r2, s1, s2)
+        if i >= 3000:
+            X1.append(x1)
+            X2.append(y1)
+    #使用x0,y0,z0作为初值，使用四阶龙格库塔对Chen混沌系统迭代，生成三个M*N的混沌序列-X3,X4,X5
+    X3, X4, X5 = [], [], []
+    dt = 0.001
+    for i in range(M*N+3000):
+        k1x, k1y, k1z = Chen_chaotic_system(x0, y0, z0)
+        k2x, k2y, k2z = Chen_chaotic_system(x0 + 0.5*dt*k1x, y0 + 0.5*dt*k1y, z0 + 0.5*dt*k1z)
+        k3x, k3y, k3z = Chen_chaotic_system(x0 + 0.5*dt*k2x, y0 + 0.5*dt*k2y, z0 + 0.5*dt*k2z)
+        k4x, k4y, k4z = Chen_chaotic_system(x0 + dt*k3x, y0 + dt*k3y, z0 + dt*k3z)
+        x0 += (dt/6)*(k1x + 2*k2x + 2*k3x + k4x)
+        y0 += (dt/6)*(k1y + 2*k2y + 2*k3y + k4y)
+        z0 += (dt/6)*(k1z + 2*k2z + 2*k3z + k4z)
+        if i >= 3000:
+            X3.append(x0)
+            X4.append(y0)
+            X5.append(z0)
 
+    Show_imgs(bits_imgs,mode='bits')
+    #对X1,X2,X3,X4,X5进行升序排序，得到排序索引序列，分别记为I1,I2,I3,I4,I5
+    I1 = np.argsort(X1)
+    I2 = np.argsort(X2)
+    I3 = np.argsort(X3)
+    I4 = np.argsort(X4)
+    I5 = np.argsort(X5)
+    Scrambling_image(bits_imgs, I1, 5)
+    Scrambling_image(bits_imgs, I2, 4)
+    Scrambling_image(bits_imgs, I3, 3)
+    Scrambling_image(bits_imgs, I4, 2)
+    Scrambling_image(bits_imgs, I5, 1)
+    
+    Show_imgs(bits_imgs,mode='bits')
+
+    scrambled_bits_imgs = Scrambling_BitPlane(bits_imgs,K)
+    
+    Show_imgs(scrambled_bits_imgs,mode='bits')
 
 if __name__ == "__main__":
     just_testmain()
--- [35]MIE/utils.py
+++ [35]MIE/utils.py
@@ -1,9 +1,11 @@
-def Twodim_logistic_map(x, y, a, b):
-    from constants import Towdim_LOGISTIC_MAP_r1, Towdim_LOGISTIC_MAP_r2, Towdim_LOGISTIC_MAP_s1, Towdim_LOGISTIC_MAP_s2
-    r1,r2,s1,s2 = Towdim_LOGISTIC_MAP_r1, Towdim_LOGISTIC_MAP_r2, Towdim_LOGISTIC_MAP_s1, Towdim_LOGISTIC_MAP_s2
+def Twodim_logistic_map(x, y, r1, r2, s1, s2):
+    if r1 is None or r2 is None or s1 is None or s2 is None:
+        from constants import Towdim_LOGISTIC_MAP_r1, Towdim_LOGISTIC_MAP_r2, Towdim_LOGISTIC_MAP_s1, Towdim_LOGISTIC_MAP_s2
+        r1,r2,s1,s2 = Towdim_LOGISTIC_MAP_r1, Towdim_LOGISTIC_MAP_r2, Towdim_LOGISTIC_MAP_s1, Towdim_LOGISTIC_MAP_s2
     x_next = r1*x*(1-x)+s1*y**2
     y_next = r2*y*(1-y)+s2*(x**2+x*y)
     return x_next, y_next
+
 
 def Chen_chaotic_system(x, y, z):
     from constants import CHEN_CHAOTIC_SYSTEM_a, CHEN_CHAOTIC_SYSTEM_b, CHEN_CHAOTIC_SYSTEM_c
@@ -14,7 +16,10 @@ def Chen_chaotic_system(x, y, z):
     return dxdt, dydt, dzdt
 
 
-def generate_sha256_sequence(input_string):
+def Generate_sha256_sequence(input_string):
+    """
+    return: 长度为32的整数列表,每个整数在0-255之间
+    """
     import hashlib
     hash_object = hashlib.sha256(input_string.encode())
     hex_dig = hash_object.hexdigest()
@@ -89,3 +94,79 @@ def Show_Chen():
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
     plt.show()
+
+
+def Generate_KeyStream(HashSeq: list[int]) -> list[float]:
+    key_stream = []
+    x1 = (HashSeq[0] ^ HashSeq[1])/255
+    y1 = (HashSeq[2] ^ HashSeq[3])/255
+    r1 = 2.75+(HashSeq[4] ^ HashSeq[5]^HashSeq[6]^HashSeq[7])/255*0.65
+    r2 = 2.75+(HashSeq[8] ^ HashSeq[9]^HashSeq[10]^HashSeq[11])/255*0.7
+    s1 = 0.15+(HashSeq[12] ^ HashSeq[13]^HashSeq[14]^HashSeq[15])/255*0.06
+    s2 = 0.13+(HashSeq[16] ^ HashSeq[17]^HashSeq[18]^HashSeq[19])/255*0.02
+    x0 = (HashSeq[20] ^ HashSeq[21]^HashSeq[22]^HashSeq[23])/255
+    y0 = (HashSeq[24] ^ HashSeq[25]^HashSeq[26]^HashSeq[27])/255
+    z0 = (HashSeq[28] ^ HashSeq[29]^HashSeq[30]^HashSeq[31])/255
+    return [x1, y1, r1, r2, s1, s2, x0, y0, z0]
+
+def Img2Bits(img):
+    import numpy as np
+    h, w = img.shape
+    bits = np.unpackbits(img.reshape(-1,1), axis=1)
+    return bits.reshape(h, w, 8)
+
+def Scrambling_image(bits_imgs,T,dim):
+    """
+    使用T对第dim维度进行像素位置打乱
+    """
+    import numpy as np
+
+    for img in bits_imgs:
+        h, w, c = img.shape
+        targetdim_flatten = img[:,:,dim-1].reshape(-1)
+        scrambled_flatten = targetdim_flatten[T]
+        img[:,:,dim-1] = scrambled_flatten.reshape(h, w)
+
+def Bits2Img(bits_img):
+    import numpy as np
+    h, w, c = bits_img.shape
+    img = np.packbits(bits_img.reshape(-1,8), axis=1)
+    return img.reshape(h, w)
+
+def Show_imgs(imgs,mode):
+    if mode=='bits':
+        i = 1
+        for bits_img in imgs:
+            import matplotlib.pyplot as plt
+            img = Bits2Img(bits_img)
+            plt.subplot(2,4,i)
+            plt.imshow(img, cmap='gray')
+            plt.axis('off')
+            i += 1
+        plt.show()
+    elif mode=='img':
+        import matplotlib.pyplot as plt
+        i = 1
+        for img in imgs:
+            plt.subplot(2,4,i)
+            plt.imshow(img, cmap='gray')
+            plt.axis('off')
+            i += 1
+        plt.show()
+
+def Scrambling_BitPlane(bits_imgs,K):
+    import numpy as np
+    #生成一个0-8*K-1的序列,并且随机打乱
+    Q = np.arange(0, 8*K)
+    np.random.shuffle(Q)
+    Bit_combine_imgs = np.concatenate(bits_imgs, axis=2)  
+
+    #对每一位平面进行打乱
+    Bit_combine_imgs_scrambled = np.empty_like(Bit_combine_imgs)
+    for i in range(8*K):
+        Bit_combine_imgs_scrambled[:,:,i] = Bit_combine_imgs[:,:,Q[i]]
+    #将打乱后的位平面重新分割为K个图像
+    scrambled_bits_imgs = []
+    for i in range(K):
+        scrambled_bits_imgs.append(Bit_combine_imgs_scrambled[:,:,i*8:(i+1)*8])
+    return scrambled_bits_imgs  
